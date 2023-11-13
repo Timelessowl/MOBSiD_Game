ccb44e693b4daa33d854e4838b59c6ef190aa16d
timelessowl <kpunenko@gmail.com>
2023-11-13 15:25:59 +0500

    register backend

--- Django_MOBSiD_Game/Django_MOBSiD_Game/settings.py
+++ Django_MOBSiD_Game/Django_MOBSiD_Game/settings.py
@@ -87,6 +87,17 @@ DATABASES = {
     }
 }
 
+##User model
+AUTH_USER_MODEL = 'api.CustomAppUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
